896ae997c9acdbc0feb14152cf453ef9eb4e80fb
Conor Doherty <doherty.conor@gmail.com>
2023-08-17 12:18:59 -0700

    make "excess et" variable for consistency, added interp function

--- water_balance_runoff.py
+++ water_balance_runoff.py
@@ -35,8 +35,9 @@ def do_wb(taw_full, pr_ts, et_ts, irrig_ts=None):
 
         dp = max(pr - ro - et - last_dr, 0)
         dr = min(max(last_dr - pr + ro + et + dp, 0), taw)
-        if last_dr + et - pr + ro > taw:
-            et_of_aw = et-(dr-last_dr+pr-ro)
+        excess_et = et-(dr-last_dr+pr-ro)
+        if excess_et > taw:
+            et_of_aw = excess_et
         else:
             et_of_aw = 0
 
@@ -62,3 +63,38 @@ def do_wb(taw_full, pr_ts, et_ts, irrig_ts=None):
         et_of_aw_ts[i] = et_of_aw
 
     return dr_ts, dp_ts, et_of_aw_ts
+
+@njit
+def etof_interp(et_ts, eto_ts, nodata=-9999.):
+    etof_ts = np.zeros_like(eto_ts)
+
+    # fill leading empty values with first EToF
+    first_et_ind = np.argmax(et_ts != nodata)
+    first_et_val = et_ts[first_et_ind]
+    first_etof = first_et_val / eto_ts[first_et_ind]
+    etof_ts[:first_et_ind+1] = first_etof
+
+    start_ind = first_et_ind
+    for i in range(first_et_ind+1, et_ts.size):
+        # find next non-missing et value
+        if et_ts[i] != nodata:
+            start_etof = et_ts[start_ind] / eto_ts[start_ind]
+            end_etof = et_ts[i] / eto_ts[i]
+
+            # linear interpolate from start to end
+            etof_ts[start_ind:i] = np.linspace(start_etof, end_etof, i-start_ind)
+
+            # set current index as start_ind
+            start_ind = i
+        # reach the end of et_ts and last value is missing
+        elif i == et_ts.size-1 and et_ts[i] == nodata:
+            etof_ts[start_ind:] = et_ts[start_ind] / eto_ts[start_ind]
+        elif et_ts[i] == nodata:
+            continue
+        else:
+            # something is wrong !!!
+            #raise Excpetion('should not be here!!!')
+            print('should not be here!!!')
+
+    # return interpolated ET
+    return etof_ts * eto_ts
